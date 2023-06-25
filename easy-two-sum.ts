c1063611e95934e2c0321d18db1536c73e852199
Dikendev <diken.dev@gmail.com>
2023-06-25 16:32:58 -0300

    two sum (easy)

---
+++ easy-two-sum.ts
@@ -0,0 +1,33 @@
+/*  
+* GET INDEX OF NUMBER WHERE TWO NUMBER FOR ARRAY EQUAL GOAL
+* @param totalNumber - array of numbers;
+* @param target - number
+* @returns
+
+arr = [2,3,5,6,7,8,9,10]
+b(o)2
+result = 8.
+
+*/
+
+const twoSum = (totalNumber: number[], target: number): number[] | undefined => {
+  let previousValues: {} = {};
+
+  for (let index = 0; index < totalNumber.length; index++) {
+    let currentNumber: number = totalNumber[index]
+    let neededValue: number = target - currentNumber;
+    let index2 = previousValues[neededValue];
+
+    if (index2 != undefined) {
+      return [index2, index];
+    }
+    else {
+      previousValues[currentNumber] = index; 
+    }
+  }
+}
+
+let array_numbers: number[] = [2,7,11,15];
+let target_numbers: number = 9;
+
+console.log(twoSum(array_numbers, target_numbers));
