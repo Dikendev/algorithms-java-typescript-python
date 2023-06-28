6c1625637797cbc7f3ca8ebbaa1a1d524349968f
Dikendev <diken.dev@gmail.com>
2023-06-27 21:28:48 -0300

    selection sort

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -1,18 +1,21 @@
 def findSmallest(arr):
-  smallest = arr[0]
-  smallest_index = 0
+  smallest = arr[0]   # < store the smallest value
+  smallest_index = 0  # < store the index of the smallest value
 
-  for i in range(1, len(arr)):
-    if arr[i] < smallest:
-      smallest = arr[i]
-      smallest_index = i
-  return smallest_index
+  for index in range(1, len(arr)):
+    if arr[index] < smallest:
+      smallest = arr[index]
+      smallest_index = index
+    
+    return smallest_index
 
 def selectionSort(arr):
   newArr = []
-  for i in range(len(arr)):
+
+  for index in range(len(arr)):  
     smallest = findSmallest(arr)
     newArr.append(arr.pop(smallest))
+  
   return newArr
 
-print(selectionSort([5, 3, 6, 2, 10]))
+print(selectionSort([5,3,6,2,10]))
