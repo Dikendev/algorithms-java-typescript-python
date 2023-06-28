861a294ec34e1e481dd97d14c1171f23ba867359
Dikendev <diken.dev@gmail.com>
2023-06-27 23:32:10 -0300

    updated selection sort (easy)

--- python/selection_sort.py
+++ python/selection_sort.py
@@ -1,21 +1,20 @@
 def findSmallest(arr):
-  smallest = arr[0]   # < store the smallest value
-  smallest_index = 0  # < store the index of the smallest value
+  smallest = arr[0]
+  smallest_index = 0
 
-  for index in range(1, len(arr)):
-    if arr[index] < smallest:
-      smallest = arr[index]
-      smallest_index = index
-    
-    return smallest_index
+  for i in range(1, len(arr)):
+    if arr[i] < smallest:
+      smallest = arr[i]
+      smallest_index = i
+  return smallest_index
 
 def selectionSort(arr):
   newArr = []
-
-  for index in range(len(arr)):  
+  for i in range(len(arr)):
     smallest = findSmallest(arr)
     newArr.append(arr.pop(smallest))
-  
   return newArr
 
-print(selectionSort([5,3,6,2,10]))
+
+unorderedArray = [5,3,6,2,10]
+print(selectionSort(unorderedArray))
