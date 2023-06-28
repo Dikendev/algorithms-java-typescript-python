3f5d208b3675c59000de8acc8b5fcdafce5dd979
Dikendev <diken.dev@gmail.com>
2023-06-27 22:18:25 -0300

    selection sort (easy)

---
+++ typescript/selection-sort.ts
@@ -0,0 +1,27 @@
+function findSmallest<T>(numberList: T[]): number {
+  let smallestIndex: number = 0;
+  let smallest: T = numberList[smallestIndex];
+
+  for (let index = 1; index < numberList.length; index++) {
+    if (numberList[index] < smallest) {
+      smallest = numberList[index];
+      smallestIndex = index;
+    }
+  }
+
+  return smallestIndex;
+}
+
+function selectionSort<T>(arrayToSort: T[]): T[] {
+  const sortedArray: T[] = [];
+  const length = arrayToSort.length;
+
+  for (let index = 0; index < length; index++) {
+    const smallestIndex: number = findSmallest(arrayToSort);
+    sortedArray.push(arrayToSort.splice(smallestIndex, 1)[0]);
+  }
+
+  return sortedArray;
+}
+
+console.log(selectionSort([9, 8, 5, 4, 2,]))
