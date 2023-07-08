82420ca6fb3a1b5ab6c0d40fb856cf59822be58d
Dikendev <diken.dev@gmail.com>
2023-07-07 22:46:08 -0300

    updated two sum (easy)

--- typescript/easy-two-sum.ts
+++ typescript/easy-two-sum.ts
@@ -1,8 +1,8 @@
-/*  
+/**
 * GET INDEX OF NUMBER WHERE TWO NUMBER FOR ARRAY EQUAL GOAL
-* @param totalNumber - array of numbers;
-* @param target - number
-* @returns
+* @param totalNumber {number[]} totalNumber {Number[]}
+* @param target {number} target Number
+* @returns {number[]} Result
 
 arr = [2,3,5,6,7,8,9,10]
 b(o)2
@@ -10,24 +10,26 @@ result = 8.
 
 */
 
-const twoSum = (totalNumber: number[], target: number): number[] | undefined => {
-  let previousValues: {} = {};
+const twoSum = (
+	totalNumber: number[],
+	target: number
+): number[] | undefined => {
+	let previousValues: any = [];
 
-  for (let index = 0; index < totalNumber.length; index++) {
-    let currentNumber: number = totalNumber[index]
-    let neededValue: number = target - currentNumber;
-    let index2 = previousValues[neededValue];
+	for (let index = 0; index < totalNumber.length; index++) {
+		let currentNumber: number = totalNumber[index];
+		let neededValue: number = target - currentNumber;
+		let index2 = previousValues[neededValue];
 
-    if (index2 != undefined) {
-      return [index2, index];
-    }
-    else {
-      previousValues[currentNumber] = index; 
-    }
-  }
-}
+		if (index2 != undefined) {
+			return [index2, index];
+		} else {
+			previousValues[currentNumber] = index;
+		}
+	}
+};
 
-let array_numbers: number[] = [2,7,11,15];
-let target_numbers: number = 9;
+const array_numbers: any[] = [2, 7, 11, 15];
+const target_numbers: number = 9;
 
 console.log(twoSum(array_numbers, target_numbers));
