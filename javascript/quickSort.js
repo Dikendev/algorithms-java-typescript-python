ea90b7d81905674fd9c2864dff182ad6482f7385
Dikendev <diken.dev@gmail.com>
2023-07-15 20:34:58 -0300

    quick sort javascript

--- javascript/quickSort.js
+++ javascript/quickSort.js
@@ -9,24 +9,18 @@ function quicksort(array) {
   if (array.length < 2) return array;
   // recursive case
   let pivot = array[0];
-  console.log(`pivot`, pivot)
   // partitioning the lefts arrays.
   // sub-arrays of all the elements less than the pivot
   let less = array.slice(1).filter(function (element) {
     return element <= pivot;
   })
-  console.log(`less`, less)
-
   //sub-array of all the elements greater than the pivot
   let greater = array.slice(1).filter(function (element) {
     return element > pivot;
   })
-  console.log(`greater`, greater)
 
   return quicksort(less).concat([pivot], quicksort(greater));
-
 }
-console.timeEnd(`quicksort`)
 
 let numbersArray = [2, 5, 7, 4, 9, 1];
 console.log(quicksort(numbersArray));
