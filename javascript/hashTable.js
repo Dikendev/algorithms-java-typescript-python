24429c4801d94ca38217f2e701d886ce3ef6a5f7
Dikendev <diken.dev@gmail.com>
2023-07-21 23:06:38 -0300

    hash table

--- javascript/hashTable.js
+++ javascript/hashTable.js
@@ -1,78 +1,16 @@
-/* internals of hash table
-implementations, collisions and hash functions.
-*/
-const book = {}
-book['apple'] = 0.67;
-book['milk'] = 2.3;
-book['avocado'] = 1.32;
-
-const phoneBook = {};
-phoneBook['cristina'] = 9992341;
-phoneBook['police'] = 190;
 
 /**
- * Vote check
- * @param {string} name Voter name
+ * Class HashTable
+ * @param {Object} obj 
  */
-const voted = {};
-function check_voter(name) {
-  if (voted[name]) {
-    console.log('get out!')
-  } else {
-    voted[name] = true;
-    console.log('let the vote', name)
-  }
+const HashTable = function (obj) {
+  let length = 0;
+  this.items = (function (obj) {
+    let items = {};
+    for (let p in obj) {
+      item[p] = obj[p];
+      length++
+    }
+    return items;
+  })(obj)
 }
-// check_voter('diego')
-// check_voter('diego')
-// check_voter('cristina')
-
-const list = {}
-function returnOne(input) {
-  if (!list[input]) {
-    console.log(1)
-  }
-}
-// returnOne('oi')
-
-/**
- * Save the name length as index
- * @param {string} name 
- */
-const listName = {};
-function nameAsIndex(name) {
-  const lengthOfName = name.length;
-  if (!listName[name]) {
-    listName[lengthOfName] = name
-    console.log('saved', listName)
-  } else {
-    console.log('already saved', listName[name])
-  }
-}
-// nameAsIndex('diego')
-console.log('obj final', listName)
-
-const savedByFirstLetter = {}
-function saveFistLetter(name) {
-  const firstLetter = name[0]
-  if (!savedByFirstLetter[name]) {
-    savedByFirstLetter[firstLetter] = name
-    console.log('saved')
-  } else {
-    console.log('already saved')
-  }
-}
-// saveFistLetter('cirstina')
-// saveFistLetter('curioso')
-
-const books = {};
-function BooksByAuthor(author, titles) {
-  if (!books[author]) {
-    books[author] = titles
-  } else {
-    console.log('This book already exist in hash')
-  }
-}
-// BooksByAuthor('watchmen', 'cristofer nollon')
-// BooksByAuthor('watchmen', 'cristofer nollon')
-// console.log(books)
