e6501aa0192a6ba448164b9c43525537ecf8caa2
Dikendev <diken.dev@gmail.com>
2023-07-23 21:51:46 -0300

    insertion sort

---
+++ javascript/insertionSort.js
@@ -0,0 +1,28 @@
+/**
+ * Running time = O(nˆ2)
+ * Ω(n) in best case
+ * Sort numbers array unsorted  to sorted using insertion alg
+ * @param {array} array of numbers unsorted 
+ * @returns {array} array of numbers sorted
+ */
+const insertionSort = array => {
+  const len = array.length;
+
+  for (let i = 1; i < len; i++) {
+    for (let j = i; j > 0; j--) {
+      if (array[j] < array[j - 1]) {
+        [array[j], array[j - 1]] = [array[j - 1], array[j]];
+      } else {
+        break;
+      }
+    }
+  }
+  return array;
+}
+
+let arrayOfNumbers = [
+  44, 12, 11, 58, 75, 31, 38,
+  51, 40, 5, 8, 72, 36, 29,
+  9, 66, 54, 8, 81, 28
+]
+console.log(insertionSort(arrayOfNumbers))
