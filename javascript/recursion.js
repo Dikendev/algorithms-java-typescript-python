084fbb54ca24fee802835cc0d125c2e639d2756a
Dikendev <diken.dev@gmail.com>
2023-08-03 20:41:58 -0300

    Flattening of Array

--- javascript/recursion.js
+++ javascript/recursion.js
@@ -1,17 +1,34 @@
 function countDown(count) {
   console.log(count);
   if (count == 0) {
-    return
+    return;
   }
-  setTimeout(() => { countDown(count - 1) }, 1000)
+  setTimeout(() => { countDown(count - 1) }, 1000);
 }
 
 let number = 3476;
 
 function digitSum(num) {
   if (num == 0) {
-    return num
+    return num;
   }
-  return num % 10 + digitSum(Math.floor(num / 10))
+  return num % 10 + digitSum(Math.floor(num / 10));
 }
 
+var arr = [2, [3, 12, 24], 17, [54, [24, 3], 8]];
+var result = [];
+
+function flattenArray(arr, i) {
+  if (i >= arr.length) {
+    return
+  }
+  if (Array.isArray(arr[i])) {
+    flattenArray(arr[i], 0);
+  } else {
+    result.push(arr[i])
+  }
+  flattenArray(arr, i + 1)
+}
+
+flattenArray(arr, 0)
+console.log(result)
