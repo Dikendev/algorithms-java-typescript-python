c435ddd2950820487ead48fe2e4be3410707e11f
Dikendev <diken.dev@gmail.com>
2023-08-06 00:50:54 -0300

    bubble sort

--- javascript/bubble-sort.js
+++ javascript/bubble-sort.js
@@ -1,23 +1,22 @@
-function bubbleSort(originalArr) {
+/**
+ * Compare each number in array to sort from lower to the highest number in array.
+ * @param {Array} originalArr - Array of unordered
+ * @returns {Array } arr - Sorted array
+ */
+const bubbleSort = originalArr => {
   const arr = [...originalArr]
-  let counter = 0
   let swapped = false;
   const swap = (arr, index1, index2) =>
     [arr[index1], arr[index2]] = [arr[index2], arr[index1]]
   for (let i = 0; i < arr.length; i++) {
-
     for (let j = i + 1; j < arr.length; j++) {
-      counter++
       if (arr[i] > arr[j]) {
         swap(arr, j, i)
         swapped = true
       }
     }
   }
-  console.log('counter', counter)
-
   return arr
 }
 const arr = [10, 9, 1, 4, 2, 3]
 console.log(bubbleSort(arr))
-console.log(arr)
