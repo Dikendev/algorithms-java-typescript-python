b068d50422f16515a75c299c57e3b0ecbc540bdc
Dikendev <diken.dev@gmail.com>
2023-09-02 22:48:49 -0300

    filter elements with map approach better clean functions

--- typescript/filter-elements-array.ts
+++ typescript/filter-elements-array.ts
@@ -1,5 +1,7 @@
 export {};
 
+type Fn = (n: number, i: number) => boolean | number;
+
 function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
 	const newNumber: number[] = [];
 	arr.map((element, index) => {
@@ -8,13 +10,7 @@ function filter(arr: number[], fn: (n: number, i: number) => any): number[] {
 	return newNumber;
 }
 let arr = [0, 10, 20, 30];
-const fn = function greaterThan10(n: number): boolean {
-	return n > 10;
-};
-const fn2 = function firstIndex(n: number, i: number): boolean {
-	return i === 0;
-};
-const fn3 = function plusOne(n: number): number {
-	return n + 1;
-};
-filter(arr, fn2);
+const greaterThan10: Fn = (n: number): boolean => n > 10;
+const firstIndex: Fn = (n: number, i: number): boolean => i === 0;
+const plusOne: Fn = (n: number): number => n + 1;
+filter(arr, firstIndex);
