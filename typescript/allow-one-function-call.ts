6ba46c1ab0bf47f7838c160bc80ed87a746311fa
Dikendev <diken.dev@gmail.com>
2023-09-04 22:12:29 -0300

    Allow One Function Call - 2666

--- typescript/allow-one-function-call.ts
+++ typescript/allow-one-function-call.ts
@@ -7,13 +7,7 @@ function once(fn: Fn): Fn {
 		return fn(...args);
 	};
 }
-/**
- * let fn = (a,b,c) => (a + b + c)
- * let onceFn = once(fn)
- *
- * onceFn(1,2,3); // 6
- * onceFn(2,3,6); // returns undefined without calling fn
- */
+
 const fn = (a: number, b: number, c: number): number => a + b + c;
 let onceFn: Fn = once(fn);
 console.log(onceFn(1, 2, 3));
