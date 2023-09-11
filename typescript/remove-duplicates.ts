a139da1c869f306f0f178c2fdf30da9ecd1f9133
Dikendev <diken.dev@gmail.com>
2023-09-11 00:04:42 -0300

    Remove duplicates from sorted array- 26

--- typescript/remove-duplicates.ts
+++ typescript/remove-duplicates.ts
@@ -2,18 +2,17 @@ export {};
 
 function removeDuplicates(nums: number[]): number {
 	let uniqueNum: number[] = [];
-	for (let i = 0; i < nums.length; i++) {
+	nums.forEach(() => {
 		uniqueNum = [...new Set(nums)];
-	}
-	if (uniqueNum) {
-		nums.unshift(...uniqueNum);
-	}
+	});
+	if (uniqueNum) nums.unshift(...uniqueNum);
 	return uniqueNum.length;
 }
 
 let numsArr: number[] = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
 const expectedArr: number[] = [1, 2, 3];
 const k = removeDuplicates(numsArr);
+console.log(k);
 
 const expectedArrLength = expectedArr.length;
 for (let i = 0; i < expectedArrLength; i++) {
