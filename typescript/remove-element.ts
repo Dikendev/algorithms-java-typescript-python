3f0e93cc5600ca3479a52ea7e03f0cb27994c13b
Dikendev <diken.dev@gmail.com>
2023-09-11 23:53:06 -0300

    Remove elements

---
+++ typescript/remove-element.ts
@@ -0,0 +1,17 @@
+export {};
+function removeElement(nums: number[], vol: number): number {
+	let counter = 0;
+
+	for (let i = 0; i < nums.length; i++) {
+		if (nums[i] !== vol) {
+			nums[counter++] = nums[i];
+		}
+	}
+	return counter;
+}
+let nums = [3, 2, 2, 3];
+const val = 3;
+
+const k = removeElement(nums, val);
+
+console.log(k);
