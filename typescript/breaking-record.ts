7bfa86eebed9e1143cbdf2ea0dfc9a67c17a7837
Dikendev <diken.dev@gmail.com>
2023-11-30 23:28:43 -0300

    breaking records :)

--- typescript/breaking-record.ts
+++ typescript/breaking-record.ts
@@ -1,6 +1,20 @@
-export { }
-function breakingRecords(scores: number[]): number[] {
-  // Write your code here
+export {};
+function breakingRecords(scores: number[]): any {
+	// Write your code here
+	let lowScore: number = 0;
+	let highScore: number = 0;
 
-  return []
+	let map2 = new Map<number, number>();
+
+	for (let i = 0; i < scores.length; i++) {
+		if (lowScore > scores[i]) {
+			lowScore = scores[i];
+		} else {
+			highScore = scores[i];
+		}
+	}
+	console.log("lowScore", lowScore);
 }
+
+const scores: number[] = [12, 24, 10, 24];
+breakingRecords(scores);
