9e664e482756127a5b3860e144cc5303e6ecc717
Dikendev <diken.dev@gmail.com>
2023-11-30 23:34:12 -0300

    sock merchant 🔥

---
+++ typescript/sock-merchant.ts
@@ -0,0 +1,7 @@
+export {};
+
+function sockMerchant(n: number, ar: number[]): number {
+	// Write your code here
+}
+const array = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+const arrayLength = array.length;
