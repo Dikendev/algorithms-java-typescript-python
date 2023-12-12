91db07ae6cc3bd27d9d8c5a5734a2fbbcabef293
Dikendev <diken.dev@gmail.com>
2023-12-11 23:43:52 -0300

    removing console.log

--- typescript/find-max.ts
+++ typescript/find-max.ts
@@ -15,7 +15,6 @@ const findMax = (arr: number[]): Record<number, number> | null => {
 			};
 
 			Object.values(maxNumberRecord);
-			console.log(`totalMax[arr[i]]`, totalMax[arr[i]]);
 			console.log(`maxNumberRecord`, maxNumberRecord);
 		} else {
 			totalMax[arr[i]] = 1;
