9e19d2d0f27b88e05817825c08911e45c4ff4388
Dikendev <diken.dev@gmail.com>
2023-12-11 23:41:40 -0300

    removing console.log

--- typescript/duplicate-zeros.ts
+++ typescript/duplicate-zeros.ts
@@ -2,8 +2,6 @@
  Do not return anything, modify arr in-place instead.
  */
 function duplicateZeros(arr: number[]): void {
-	console.log("arr", arr);
-
 	for (let i = 0; i < arr.length; i++) {
 		if (arr[i] === 0) {
 			for (let j = arr.length - 1; j > i; j--) {
