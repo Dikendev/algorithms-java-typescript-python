f0dcc6de89bb25e62b84e8e6cccfdb8c94987869
Dikendev <diken.dev@gmail.com>
2023-12-12 23:44:52 -0300

    removing console.log

--- typescript/is-array-good.ts
+++ typescript/is-array-good.ts
@@ -1,14 +1,6 @@
 export {};
 
 /**
- * The `isGood` function takes an array of numbers as input and performs some operations on it.
- * It keeps track of the highest number encountered so far using a `higher` object.
- * The function then iterates through each number in the input array and checks if it is already present in the `higher` object.
- * If it is, the function compares the current number with the value stored in the `higher` object.
- * If the current number is greater, it updates the value in the `higher` object.
- * If the current number is smaller, it logs a message indicating that the current number is smaller.
- * If the number is not present in the `higher` object, it adds it with the current number as the value.
- * Finally, the function logs the `higher` object after all iterations are completed.
  *
  * @param nums - An array of numbers
  */
