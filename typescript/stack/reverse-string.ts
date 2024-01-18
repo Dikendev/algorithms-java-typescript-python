ab6c6a10baa75f68eb0b9581e35a0a16a88b228a
Dikendev <diken.dev@gmail.com>
2024-01-18 20:54:25 -0300

    reverse string

--- typescript/stack/reverse-string.ts
+++ typescript/stack/reverse-string.ts
@@ -1,15 +1,18 @@
 export {};
 
 function reverseString<T>(word: string): string {
-	let stack: T[] = [];
+	let stack: string[] = [];
 
-	for (let i = 0; i < word.length; i++) {}
-
+	for (let i = 0; i < word.length; i++) {
+		stack.push(word[i]);
+	}
 	let reverseString = "";
 
 	while (stack.length > 0) {
-		reverseString += stack.pop;
+		reverseString += stack.pop();
 	}
-
 	return reverseString;
 }
+
+const word = "abra kadabra";
+console.log(reverseString(word));
