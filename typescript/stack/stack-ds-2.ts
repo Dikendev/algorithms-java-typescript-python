578af8ae20724f7a4cb40d76db75d2f9bcb9db0d
Dikendev <diken.dev@gmail.com>
2024-01-18 00:27:24 -0300

    Stack ds

--- typescript/stack/stack-ds-2.ts
+++ typescript/stack/stack-ds-2.ts
@@ -7,10 +7,12 @@ s.push(3);
 s.push(4);
 s.push(5);
 s.push(30);
-s.push(2);
+s.push("A");
 
 console.log(s.size());
 console.log(s.peek());
 console.log(s.pop());
-console.log(s.clear());
 console.log(s.size());
+console.log(s.peek());
+console.log(s.clear());
+console.log(s.peek());
