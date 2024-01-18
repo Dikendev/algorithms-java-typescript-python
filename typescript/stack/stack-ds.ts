324d606ae8d34c5bba74f6e7abe1756258fa8c82
Dikendev <diken.dev@gmail.com>
2024-01-17 23:50:55 -0300

    Stack ds

--- typescript/stack/stack-ds.ts
+++ typescript/stack/stack-ds.ts
@@ -1,1 +1,33 @@
 export {};
+
+export class Stack<T> {
+	private dataStore: T[] = [];
+	top: number;
+	len: number = 0;
+
+	constructor() {
+		this.dataStore = [];
+		this.top = 0;
+	}
+
+	push(element: T): void {
+		this.dataStore[this.top++] = element;
+	}
+
+	pop(): T {
+		return this.dataStore[--this.top];
+	}
+
+	peek(): T {
+		return this.dataStore[this.top - 1];
+	}
+
+	size() {
+		return this.top;
+	}
+
+	clear() {
+		this.top = 0;
+		this.dataStore.length = 0;
+	}
+}
