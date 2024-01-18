07df030246571c9f7003d57c41e4661c19e1e7a6
Dikendev <diken.dev@gmail.com>
2024-01-17 23:59:23 -0300

    Stack ds

--- typescript/stack/stack-ds-2.ts
+++ typescript/stack/stack-ds-2.ts
@@ -1,3 +1,16 @@
 import { Stack } from "./stack-ds";
 
 const s = new Stack();
+s.size();
+s.push(2);
+s.push(3);
+s.push(4);
+s.push(5);
+s.push(30);
+s.push(2);
+
+console.log(s.size());
+console.log(s.peek());
+console.log(s.pop());
+console.log(s.clear());
+console.log(s.size());
