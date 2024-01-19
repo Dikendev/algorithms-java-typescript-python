1c1624f6e60e3d09b64e575e0f7571a55fd54608
Dikendev <diken.dev@gmail.com>
2024-01-18 21:25:42 -0300

    Queue ds

--- typescript/queue/queue.ts
+++ typescript/queue/queue.ts
@@ -1,7 +1,34 @@
+export {};
 //FIFO (first in first out)
 
 interface Queue<T> {
 	enqueue(element: T): void;
-	dequeue(): T;
+	dequeue(): T | undefined;
 	size(): number;
 }
+
+class Queue<T> implements Queue<T> {
+	private storage: T[] = [];
+
+	constructor() {}
+
+	enqueue(element: T): void {
+		this.storage.push(element);
+	}
+
+	dequeue(): T | undefined {
+		return this.storage.shift();
+	}
+
+	size(): number {
+		return this.storage.length;
+	}
+}
+
+const queue = new Queue<string>();
+
+queue.enqueue("A");
+queue.enqueue("B");
+
+console.log(queue.size());
+console.log(queue.dequeue());
