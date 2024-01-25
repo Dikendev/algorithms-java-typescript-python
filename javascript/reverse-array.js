1dd2fd9b887560c74879cd8c7054a761eb4854c2
Dikendev <diken.dev@gmail.com>
2024-01-24 22:51:53 -0300

    reverse string

---
+++ javascript/reverse-array.js
@@ -0,0 +1,11 @@
+function reverse(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    let temp = arr[i]
+    arr[i] = arr[arr.length - i - 1]
+    arr[arr.length - i - 1] = temp
+  }
+}
+
+const array = [50, 60, 70, 80, 90]
+reverse(array)
+console.log(array)
