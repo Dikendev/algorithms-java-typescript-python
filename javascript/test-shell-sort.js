d653e7fe4e630bdc171e0596e9aefa8ddc037523
Dikendev <diken.dev@gmail.com>
2024-01-29 23:29:04 -0300

    shell sort

---
+++ javascript/test-shell-sort.js
@@ -0,0 +1,26 @@
+function shellSort(array) {
+  let middle = parseInt(array.length / 2);
+
+  for (let gap = middle; gap > 0; gap = parseInt(gap / 2)) {
+    for (let i = gap; i < array.length; i++) {
+      let j = i;
+
+      while (j - gap >= 0 && array[j] < array[j - gap]) {
+        swap(array, j, j - gap);
+        j = j - gap;
+      }
+    }
+  }
+}
+
+function swap(array, a, b) {
+  array[a] = array[a] + array[b];
+  array[b] = array[a] - array[b];
+  array[a] = array[a] - array[b]
+}
+
+let scores = [9, 6, 5, 4, 8, 0, 7, 4, 3, 1, 2]
+
+shellSort(scores)
+
+console.log(scores)
