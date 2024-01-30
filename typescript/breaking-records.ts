1c28f52640738a7e64f680082eb7f162e831dd5f
Dikendev <diken.dev@gmail.com>
2024-01-29 23:29:17 -0300

    breaking-record

--- typescript/breaking-records.ts
+++ typescript/breaking-records.ts
@@ -1,27 +1,27 @@
 function breakingRecords(scores: number[]): number[] {
-	let minCounter: number = 0;
-	let maxCounter: number = 0;
+  let minCounter: number = 0;
+  let maxCounter: number = 0;
 
-	let lowerScore: number = 0;
-	let higherScore: number = 0;
+  let lowerScore: number = 0;
+  let higherScore: number = 0;
 
-	for (let i = 0; i < scores.length; i++) {
-		if (i === 0) {
-			lowerScore = scores[i];
-			higherScore = scores[i];
-		}
+  for (let i = 0; i < scores.length; i++) {
+    if (i === 0) {
+      lowerScore = scores[i];
+      higherScore = scores[i];
+    }
 
-		if (scores[i] > higherScore) {
-			maxCounter++;
-			higherScore = scores[i];
-		}
+    if (scores[i] > higherScore) {
+      maxCounter++;
+      higherScore = scores[i];
+    }
 
-		if (scores[i] < lowerScore) {
-			minCounter++;
-			lowerScore = scores[i];
-		}
-	}
-	return [maxCounter, minCounter];
+    if (scores[i] < lowerScore) {
+      minCounter++;
+      lowerScore = scores[i];
+    }
+  }
+  return [maxCounter, minCounter];
 }
 
 let arrGames = [];
