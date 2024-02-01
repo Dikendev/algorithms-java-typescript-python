e74e1d54785c25f89de3c2d0663c577e7496531c
Dikendev <diken.dev@gmail.com>
2024-01-31 23:09:01 -0300

    unidirectional linked list

--- javascript/unidirectional-linked-list.js
+++ javascript/unidirectional-linked-list.js
@@ -36,6 +36,17 @@ class LinkedList {
     this.tail = newNode;
   }
 
+  insert(insertPosition, newNode) {
+    let p = this.head;
+    let i = 0;
+    while (p.next != null && i < insertPosition - 1) {
+      p = p.next;
+      i++
+    }
+    newNode.next = p.next;
+    p.next = newNode
+  }
+
   print(node) {
     let p = node;
 
@@ -53,4 +64,6 @@ const linkedList = new LinkedList()
 const head = linkedList.init()
 
 linkedList.add(new Node("Walnut", null))
+linkedList.insert(2, new Node("Walnut", null))
+
 linkedList.print(head)
