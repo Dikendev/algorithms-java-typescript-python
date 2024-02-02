b49942ff1fbc6382b5512163a222f9848a2a024f
Dikendev <diken.dev@gmail.com>
2024-02-01 22:53:23 -0300

    double linked list

--- javascript/double-linked-list.js
+++ javascript/double-linked-list.js
@@ -9,3 +9,28 @@ class Node {
     return this.data;
   }
 }
+
+class DoubleLinkList {
+  init() {
+    this.head = new Node("San Francisco");
+    this.head.prev = null;
+    this.head.next = null;
+
+
+    let nodeOakland = new Node("Oakland");
+    nodeOakland.prev = this.head;
+    nodeOakland.next = null;
+    this.head.next = nodeOakland;
+
+    let nodeBerkeley = new Node("Berkeley");
+    nodeBerkeley.prev = nodeOakland;
+    nodeBerkeley.next = null;
+    nodeOakland.next = nodeBerkeley;
+
+    this.tail = new Node("Fremont");
+    this.tail.prev = nodeBerkeley;
+    this.tail.next = null;
+    nodeBerkeley.next = this.tail;
+    return this.head;
+  }
+}
