9fd74b60a5cf3d03eb0a011255a60452c63b8a42
Dikendev <diken.dev@gmail.com>
2024-02-04 23:35:17 -0300

    birthday 🔥

--- typescript/birthday.ts
+++ typescript/birthday.ts
@@ -1,6 +1,5 @@
 export {};
 function birthday(s: number[], d: number, m: number): number {
-	// Write your code here
 	let counter: number = 0;
 	let counts = 0;
 	for (let i = 0; i < s.length; i++) {
@@ -9,17 +8,8 @@ function birthday(s: number[], d: number, m: number): number {
 		if (m > 1) {
 			for (let j = 1; j < s.length - 1; j++) {
 				numsToSum.push(s[j]);
-				console.log(j, "jssssss");
-
 				if (j === m) {
-					console.log(j, "j", "m", m);
-
 					const sum = numsToSum.reduce((a, b) => a + b, s[i]);
-					console.log(numsToSum, "numsToSum");
-					console.log(s[i], "s[i]");
-					console.log(s[j], "s[j]");
-
-					console.log(sum, "sum");
 
 					if (sum === d) {
 						counts++;
@@ -35,7 +25,6 @@ function birthday(s: number[], d: number, m: number): number {
 		}
 		counter++;
 	}
-	console.log("counter", counter);
 	return counts;
 }
 const segment = [2, 5, 1, 3, 4, 4, 3, 5, 1, 1, 2, 1, 4, 1, 3, 3, 4, 2, 1];
