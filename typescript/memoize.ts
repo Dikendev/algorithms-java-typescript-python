3af543f1bff576892259ccbf9d3067a5da382c4e
Dikendev <diken.dev@gmail.com>
2024-02-05 22:57:36 -0300

    memoize

--- typescript/memoize.ts
+++ typescript/memoize.ts
@@ -17,14 +17,3 @@ const empty: number[] = [];
 const memoizedFn = memoize(fib);
 memoizedFn(5);
 memoizedFn(empty);
-
-/**
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1
- */
