6daa0a9b3ac70d452d8fc68662928c945cb8ee09
Dikendev <diken.dev@gmail.com>
2024-02-16 23:59:35 -0300

    removing console.log

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -2,7 +2,6 @@ function selectionSort(arr) {
 
   let minIndex = 0;
   for (let i = 0; i < arr.length - 1; i++) {
-    console.log(minIndex)
     minIndex = i
     let minValue = arr[minIndex]
     for (let j = i; j < arr.length; j++) {
@@ -19,9 +18,7 @@ function selectionSort(arr) {
 
     return minValue
   }
-  //
 }
 
 const array = [60, 80, 95, 50, 70]
 console.log(selectionSort(array))
-console.log(array)
