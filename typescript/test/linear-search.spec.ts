4d8f522c900183856da9619d5c6d74c8a8720b4d
Dikendev <diken.dev@gmail.com>
2024-03-08 22:26:59 -0300

    test add when value is found case

--- typescript/test/linear-search.spec.ts
+++ typescript/test/linear-search.spec.ts
@@ -1,7 +1,13 @@
 import linearSearch from "../linear-search";
 
+const array = [1, 3, 4, 5];
+
 describe("Linear search", () => {
 	it("should return null when value is not found", () => {
-		expect(linearSearch([1, 3, 4, 5], 6)).toBeNull();
+		expect(linearSearch(array, 6)).toBeNull();
+	});
+
+	it("should return the index when value is found", () => {
+		expect(linearSearch(array, 5)).toBe(3);
 	});
 });
