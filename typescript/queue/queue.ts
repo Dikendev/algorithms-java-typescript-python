3f2c625b347f721a19bb9dfdd3fb3e13603d41e7
Dikendev <diken.dev@gmail.com>
2024-03-12 23:31:03 -0300

    feat add queue

--- typescript/queue/queue.ts
+++ typescript/queue/queue.ts
@@ -1,34 +1,49 @@
 export {};
-//FIFO (first in first out)
 
-interface Queue<T> {
+/**
+ * FIFO (first in first out)
+ */
+export interface Queue<T> {
 	enqueue(element: T): void;
-	dequeue(): T | undefined;
+	dequeue(): T;
 	size(): number;
 }
 
-class Queue<T> implements Queue<T> {
+export class Queue<T> implements Queue<T> {
+	protected MAX_QUEUE_SIZE: number = 1000;
+	protected head: number = 0;
 	private storage: T[] = [];
 
-	constructor() {}
-
-	enqueue(element: T): void {
-		this.storage.push(element);
+	constructor(MAX_QUEUE_SIZE?: number) {
+		if (typeof MAX_QUEUE_SIZE === "number") {
+			this.MAX_QUEUE_SIZE = MAX_QUEUE_SIZE;
+		}
 	}
 
-	dequeue(): T | undefined {
-		return this.storage.shift();
+	enqueue(element: T): void {
+		if (this.size() < this.MAX_QUEUE_SIZE) {
+			console.log("this.MAX_QUEUE_SIZE, ", this.MAX_QUEUE_SIZE);
+
+			console.log("sizê, ", this.size());
+			this.storage.push(element);
+		} else {
+			throw new Error("Queue overflow!");
+		}
+	}
+
+	dequeue(): T {
+		const output = this.storage[this.head];
+		this.storage[this.head] = undefined as unknown as T;
+		this.head++;
+
+		if (this.head >= this.MAX_QUEUE_SIZE) {
+			this.storage.slice(0, this.head);
+			this.head = 0;
+		}
+		return output;
 	}
 
 	size(): number {
-		return this.storage.length;
+		return this.storage.length - this.head;
 	}
 }
-
-const queue = new Queue<string>();
-
-queue.enqueue("A");
-queue.enqueue("B");
-
-console.log(queue.size());
-console.log(queue.dequeue());
