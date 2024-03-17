b8bd8847087c4a67b6ee5d3ff47fd27ba12e1a02
Dikendev <diken.dev@gmail.com>
2024-03-16 23:56:03 -0300

    test hash table put element

---
+++ typescript/test/hash-table.spec.ts
@@ -0,0 +1,14 @@
+import { HashTable } from "../hash-table/hash-table";
+
+describe("HashTable", () => {
+	it("should put a key", () => {
+		const hashTable = new HashTable<string, number>(
+			10,
+			(value: string) => value.length
+		);
+
+		hashTable.put("one", 1);
+		const elements = hashTable.elements();
+		console.log("element", elements);
+	});
+});
