90bef3bbe158499437e287544c677ce5939bf972
Dikendev <diken.dev@gmail.com>
2024-03-26 22:10:29 -0300

    generics power

--- typescript/algorithms/dictionary/generics.ts
+++ typescript/algorithms/dictionary/generics.ts
@@ -1,29 +1,29 @@
-const phoneList = [
-	{ customerId: "0001", areaCode: "321", number: "555-1234" },
-	{ customerId: "0002", areaCode: "321", number: "555-1235" },
-	{ customerId: "0003", areaCode: "321", number: "555-1236" },
-	{ customerId: "0004", areaCode: "321", number: "555-1237" },
-	{ customerId: "0005", areaCode: "321", number: "555-1238" },
-];
-
-interface PhoneInfo {
-	customerId: string;
-	areaCode: string;
-	number: string;
+export interface OrderInfo {
+	id: string;
+	orderNumber: string;
+	name: string;
 }
 
-interface Dictionary<T> {
+export interface ContactInfo {
+	name: string;
+	number: string;
+	address: string;
+}
+
+export interface Dictionary<T> {
 	[key: string]: T;
 }
 
-function listToDict<T>(list: T[], idGen: (arg: T) => string): Dictionary<T> {
+export function listToDict<T>(
+	list: T[],
+	idGen: (arg: T) => string
+): Dictionary<T> {
 	const dictionary: Dictionary<T> = {};
 
 	list.forEach((item: T) => {
 		const dictionaryKey = idGen(item);
 		dictionary[dictionaryKey] = item;
 	});
+
 	return dictionary;
 }
-
-console.log(listToDict(phoneList, (item: PhoneInfo) => item.customerId));
