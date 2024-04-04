b702aadcff69c193f528dfb43b4becbae67a3c80
Dikendev <diken.dev@gmail.com>
2024-04-04 00:03:23 -0300

    probability initial

---
+++ python/data-science/probability.py
@@ -0,0 +1,6 @@
+def probability(): 
+  sample_space = {'Heads', 'Tails'}
+  probability_heads = 1 / len(sample_space)
+  print(f'Probability of choosing heads is {probability_heads}')
+  
+probability()
