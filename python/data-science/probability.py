f301e1473b23be739fa6c0866dafb31a07ca49cf
Dikendev <diken.dev@gmail.com>
2024-04-05 19:52:36 -0300

    feat add event detection function

--- python/data-science/probability.py
+++ python/data-science/probability.py
@@ -1,6 +1,35 @@
-def probability(): 
-  sample_space = {'Heads', 'Tails'}
-  probability_heads = 1 / len(sample_space)
+SAMPLE_SPACE = {'Heads', 'Tails'}
+
+def probability(space): 
+  probability_heads = 1 / len(space)
   print(f'Probability of choosing heads is {probability_heads}')
   
-probability()
+def is_heads_or_tails(outcome): return outcome in SAMPLE_SPACE
+def is_neither(outcome): return not is_heads_or_tails(outcome)
+
+def is_heads(outcome): return outcome ==  'Heads'
+def is_tails(outcome): return outcome == 'Tails'
+
+def get_matching_event(event_condition, sample_space):
+  return set([outcome for outcome in sample_space if event_condition(outcome)])
+
+event_conditions = [is_heads_or_tails, is_heads, is_heads, is_heads, is_neither,is_tails]
+
+def conditions():
+  for event_condition in event_conditions:
+    print(f"Event Condition: {event_condition.__name__}")
+    event = get_matching_event(event_condition,SAMPLE_SPACE)
+    print(f"Event: {event}\n")
+
+
+def compute_probability(event_condition, generic_sample_space):
+  event = get_matching_event(event_condition, generic_sample_space)
+  return len(event) / len(generic_sample_space)
+
+def eventProbability():
+  for event_condition in event_conditions:
+    prob = compute_probability(event_condition, SAMPLE_SPACE)
+    name = event_condition.__name__
+    print(f"Probability of event arising from '{name}' is {prob}")
+
+eventProbability()
