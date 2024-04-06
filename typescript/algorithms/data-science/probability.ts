eb2178d58f0c720d44afd161bf82808c86e90743
Dikendev <diken.dev@gmail.com>
2024-04-06 20:33:59 -0300

    add probability sample space

--- typescript/algorithms/data-science/probability.ts
+++ typescript/algorithms/data-science/probability.ts
@@ -1,10 +1,39 @@
+export {};
 const SAMPLE_SPACE = ["heads", "tails"] as const;
 
 export type SampleSpace = (typeof SAMPLE_SPACE)[keyof typeof SAMPLE_SPACE];
+export type Spaces = SampleSpace[];
 
-function probability(space: SampleSpace[]) {
+function probability(space: Spaces) {
 	const probabilityHeads = 1 / space.length;
 	console.log(`Probability of choosing heads is ${probabilityHeads}`);
 }
 
-probability(["heads", "tails"]);
+const ishHeadsOrTails = (): Spaces => {
+	return Object.values(SAMPLE_SPACE);
+};
+
+const isNeither = (): [] => {
+	return [];
+};
+
+const isHeads = (): SampleSpace => "heads";
+const isTails = (): SampleSpace => "tails";
+
+function getMatchingEvent(event: () => SampleSpace | Spaces) {
+	if (typeof event === "function") {
+		return event();
+	}
+}
+
+const eventConditions = [ishHeadsOrTails, isHeads, isHeads, isTails, isNeither];
+
+function conditions() {
+	for (let eventCondition of eventConditions) {
+		console.log(eventCondition.name);
+		const event = getMatchingEvent(eventCondition);
+		console.log("event", event);
+	}
+}
+
+function computeProbability() {}
