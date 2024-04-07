0c3bcaaf1359bac38e6e1fb239b807b458981188
Dikendev <diken.dev@gmail.com>
2024-04-07 11:24:00 -0300

    fix missing spaces

--- python/data-science/probability.py
+++ python/data-science/probability.py
@@ -7,20 +7,21 @@ def probability(space):
 def is_heads_or_tails(outcome): return outcome in SAMPLE_SPACE
 def is_neither(outcome): return not is_heads_or_tails(outcome)
 
+
 def is_heads(outcome): return outcome ==  'Heads'
 def is_tails(outcome): return outcome == 'Tails'
 
+event_conditions = [is_heads_or_tails, is_heads, is_heads, is_heads, is_neither,is_tails]
+
+
 def get_matching_event(event_condition, sample_space):
   return set([outcome for outcome in sample_space if event_condition(outcome)])
-
-event_conditions = [is_heads_or_tails, is_heads, is_heads, is_heads, is_neither,is_tails]
 
 def conditions():
   for event_condition in event_conditions:
     print(f"Event Condition: {event_condition.__name__}")
     event = get_matching_event(event_condition,SAMPLE_SPACE)
     print(f"Event: {event}\n")
-
 
 def compute_probability(event_condition, generic_sample_space):
   event = get_matching_event(event_condition, generic_sample_space)
@@ -54,3 +55,4 @@ for event_condition in event_conditions:
   prob = compute_event_probability(event_condition, weighted_sample_space)
   name = event_condition.__name__
   print(f"Probability of event arising from '{name} is {prob}'")
+
