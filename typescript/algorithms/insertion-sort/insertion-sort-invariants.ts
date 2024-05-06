4cf71131798683a992dbab78ec994ad1fade4f4c
Dikendev <diken.dev@gmail.com>
2024-05-06 20:21:45 -0300

    insertion sort invariant

--- typescript/algorithms/insertion-sort/insertion-sort-invariants.ts
+++ typescript/algorithms/insertion-sort/insertion-sort-invariants.ts
@@ -1,13 +1,10 @@
 export {};
 
 //loop invariants
-function insertionSort(numbers: number[], n: number) {
+export function insertionSort(numbers: number[], n: number) {
 	for (let i = 1; i < n; i++) {
 		const key = numbers[i];
 		let j = i - 1;
-
-		console.log("j", j);
-		console.log("key", key);
 
 		while (j >= 0 && numbers[j] > key) {
 			numbers[j + 1] = numbers[j];
@@ -15,8 +12,4 @@ function insertionSort(numbers: number[], n: number) {
 			numbers[j + 1] = key;
 		}
 	}
-
-	console.log(numbers);
 }
-
-insertionSort([5, 4, 3, 2, 1], 5);
