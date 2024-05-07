8e85d9a01b53f78eaf2d6742cbbda738376e854d
Dikendev <diken.dev@gmail.com>
2024-05-06 23:35:20 -0300

    insertion sort

---
+++ typescript/algorithms/insertion-sort/tests/insertion-sort.spec.ts
@@ -0,0 +1,12 @@
+import { insertionSort } from "../insertion-sort-invariants";
+
+describe("Insertion Sort", () => {
+	it("should sort an array with unique numbers", () => {
+		const numbers = [5, 7, 4, 2, 8, 1, 3, 6];
+
+		insertionSort(numbers, numbers.length);
+
+		const expected = [1, 2, 3, 4, 5, 6, 7, 8];
+		expect(numbers).toEqual(expected);
+	});
+});
