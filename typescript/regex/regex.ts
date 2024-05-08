5bde18d772517237524d384f3b8c007d92f52bd5
Dikendev <diken.dev@gmail.com>
2024-05-07 23:54:49 -0300

    regex

--- typescript/regex/regex.ts
+++ typescript/regex/regex.ts
@@ -2,5 +2,9 @@ const email: string = "diken.dev@gmail.com";
 
 function checkEmail(email: string) {
 	const regex: RegExp = /abc/;
+
 	return regex.test(email);
 }
+
+const patt = /^auto$/;
+console.log(patt.test("auto s12"));
