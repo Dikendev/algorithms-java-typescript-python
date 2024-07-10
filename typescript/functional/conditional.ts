4cb573c5df7e765bf4b6f7fa6816b10c514e01d0
Dikendev <diken.dev@gmail.com>
2024-07-09 21:06:52 -0300

    reverse type compile time

--- typescript/functional/conditional.ts
+++ typescript/functional/conditional.ts
@@ -4,3 +4,8 @@ type Y = IsString<2>;
 
 type Head<T> = T extends [infer H, ...infer _] ? H : never;
 type Z = Head<[]>;
+
+type Reverse<A> = A extends [infer First, ...infer Rest]
+	? [...Reverse<Rest>, First]
+	: A;
+type R = Reverse<[1, 2, 3, 4, 5]>;
