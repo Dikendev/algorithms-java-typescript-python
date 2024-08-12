6bb1e10bcef44b058745002810db538d285ed259
Dikendev <diken.dev@gmail.com>
2024-08-11 23:43:58 -0300

    add binary search

--- java/BinarySearch.java
+++ java/BinarySearch.java
@@ -24,7 +24,7 @@ public class BinarySearch {
       return index;
     }
 
-    public static  void main(String[] args) {
+    public static void main(String[] args) {
       int[] numbers = {1,2,3,4,5,6,7,8,9,11,15};
       int numberToFind = 16;
 
